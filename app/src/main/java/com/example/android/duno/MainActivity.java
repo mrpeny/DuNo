4b25a16955bac6daf97a6d8fead54e4c2a2eef0c
mrpeny <happypeny@gmail.com>
2017-02-13 10:44:06 +0100

    User interface finished. Test methods in java

--- app/src/main/java/com/example/android/duno/MainActivity.java
+++ app/src/main/java/com/example/android/duno/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.android.duno;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void notifyDudes(View view) {
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("*/*");
+        String[] addresses = {"happypeny@gmail.com", "bencekonya@gmail.com"};
+        intent.putExtra(Intent.EXTRA_EMAIL, addresses);
+        intent.putExtra(Intent.EXTRA_SUBJECT, getResources().getString(R.string.app_name));
+        intent.putExtra(Intent.EXTRA_TEXT, "Szevasztok Köcsögök!");
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
     }
 }
